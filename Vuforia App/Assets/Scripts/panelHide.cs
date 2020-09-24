0b731776c450da59a5408d68bb0ae7eb6b45dfeb
johnjannone16 <68834847+johnjannone16@users.noreply.github.com>
2020-09-23 23:48:18 -0400

    added double tap but it aint workin yet

---
+++ Vuforia App/Assets/Scripts/panelHide.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelHide : MonoBehaviour
+{
+    public GameObject Panel;
+    private int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.SetActive(false);
+    }
+
+
+    public void showHidePanel()
+    {
+        counter++;
+        if(counter % 2 == 1)
+        {
+            Panel.gameObject.SetActive(false);
+        }
+        else
+        {
+            Panel.gameObject.SetActive(true);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
